f22df96c3427560adde6bd8f78297f48d47c0d7f
Фёдоров Алексей Геннадьевич <fjodorov.a@kruger.loc>
2024-12-13 15:49:02 +0700

    Возвращен метод Эйлера. При реализации Рунге-Кутты Fmax снижается в 2 раза. Параметры модуля WIDTH - ширина выходного сигнала, N - определяет количество точек на период Nточек = 6.28*2**N. 2 < N < WIDTH

--- sin_cos.sv
+++ sin_cos.sv
@@ -1,6 +1,6 @@
 
                  
-module sin_cos #(parameter WIDTH = 32, N = 10) 
+module sin_cos #(parameter WIDTH = 16, N = 5) 
               (input          logic                                clk,
                input          logic                                reset,
                output         logic    signed    [WIDTH-1 : 0]     sin,
@@ -8,20 +8,20 @@ module sin_cos #(parameter WIDTH = 32, N = 10)
               );  
               
     localparam  R = {1'b0, 1'b1, {(WIDTH - 3){1'b0}}};     
-	
-	 localparam END_CYCLE = (((2**N)*628)/100)+1 ;         // the number of points in the  cycle 
-	 logic [$clog2(END_CYCLE) : 0] counter;
-	 logic rst; 
+   
+    localparam END_CYCLE = (((2**N)*628)/100)+1 ;         // the number of points in the  cycle 
+    logic [$clog2(END_CYCLE) : 0] counter;
+    logic rst; 
     
-	
-	 assign rst = reset | (~(|counter) );
-	 
+   
+    assign rst = reset | (~(|counter) );
+    
     always_ff @(posedge clk, posedge rst) begin
       if (rst) begin
         sin   <= '0;
         cos   <=  R;
-		  counter <= END_CYCLE ;
-		
+        counter <= END_CYCLE ;
+      
       end
       else begin
         counter  <=  counter - 1;
@@ -30,6 +30,6 @@ module sin_cos #(parameter WIDTH = 32, N = 10)
       end
     
     end
-	 
+    
 
 endmodule 
--- tb_sin_cos.sv
+++ tb_sin_cos.sv
@@ -12,91 +12,91 @@ module tb_sin_cos(output logic clk );
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_2),
-				               .cos(cos_16_2)
-				          );      
-							 
+                           .cos(cos_16_2)
+                      );      
+                      
   sin_cos #(16, 3) dut_16_3(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_3),
-				               .cos(cos_16_3)
-				          );      
-								 
+                           .cos(cos_16_3)
+                      );      
+                         
 
-	 sin_cos #(16, 4) dut_16_4(
+    sin_cos #(16, 4) dut_16_4(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_4),
-				               .cos(cos_16_4)
-				          );      
+                           .cos(cos_16_4)
+                      );      
 
-							 
+                      
     sin_cos #(16, 5) dut_16_5(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_5),
-				               .cos(cos_16_5)
-				          );      
-	
- 	sin_cos #(16, 6) dut_16_6(
+                           .cos(cos_16_5)
+                      );      
+   
+   sin_cos #(16, 6) dut_16_6(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_6),
-				               .cos(cos_16_6)
-				          );      
+                           .cos(cos_16_6)
+                      );      
 
-							 
+                      
     sin_cos #(16, 7) dut_16_7(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_7),
-				               .cos(cos_16_7)
-				          );      
-							 
+                           .cos(cos_16_7)
+                      );      
+                      
  sin_cos #(16, 8) dut_16_8(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_8),
-				               .cos(cos_16_8)
-				          );      
+                           .cos(cos_16_8)
+                      );      
 
-							 
+                      
   sin_cos #(16, 9) dut_16_9(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_16_9),
-				               .cos(cos_16_9)
-				          );      
-	
-							
-							
+                           .cos(cos_16_9)
+                      );      
+   
+                     
+                     
 
    sin_cos #(8, 2) dut_8_2(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_8_2),
-				               .cos(cos_8_2)
-				          );      
+                           .cos(cos_8_2)
+                      );      
   
   sin_cos #(8, 3) dut_8_3(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_8_3),
-				               .cos(cos_8_3)
-				          ); 
-							 
+                           .cos(cos_8_3)
+                      ); 
+                      
   sin_cos #(8, 4) dut_8_4(
                            .clk(clk),
                            .reset(reset),
                            .sin(sin_8_4),
-				               .cos(cos_8_4)
-				          ); 
-	  
+                           .cos(cos_8_4)
+                      ); 
+     
   
   initial begin
     #00    reset = 1;
            clk   = 0;
-    #20    reset = 0;   	
+    #20    reset = 0;      
   
     
   end
